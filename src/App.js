ea0e839de41c7f03676a09743c30a4e74e98b2fe
Sandra Padua <sandrapadua5@gmail.com>
2019-04-08 16:33:00 +0200

    created topbar component and imported in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Topbar from './components/layout/Topbar'
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <nav>
+          <Topbar/>
+        </nav>
       </div>
     );
   }
