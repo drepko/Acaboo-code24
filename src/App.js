b1b2132c10765972fe8e5f7c584d316a46236884
JewelBreeveld <rjbreeveld@gmail.com>
2019-04-08 16:41:44 +0200

    setup routes to about & courses in AppComponent

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <nav>
+          {/* TopBar */}
+        </nav>
+        <main>
+          <Route exact path="/courses" component={CoursesCountainer} />
+          <Route exact path="/about" component={AboutCountainer} />
+        </main>
       </div>
     );
   }
