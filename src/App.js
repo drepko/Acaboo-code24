f64284d539e5117db88569ece817b9fc299c1af5
JewelBreeveld <rjbreeveld@gmail.com>
2019-04-08 16:42:11 +0200

    setup routes to about & courses in AppComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Topbar from './components/layout/Topbar'
+import { Route } from 'react-router-dom'
+import { CoursePageCountainer } from './components/CoursePageContainer'
+import { AboutPageCountainer } from './components/AboutPageCountainer'
 
 
 class App extends Component {
@@ -8,11 +11,11 @@ class App extends Component {
     return (
       <div className="App">
         <nav>
-          {/* TopBar */}
+          <Topbar/>
         </nav>
         <main>
-          <Route exact path="/courses" component={CoursesCountainer} />
-          <Route exact path="/about" component={AboutCountainer} />
+          <Route exact path="/courses" component={CoursePageCountainer} />
+          <Route exact path="/about" component={AboutPageCountainer} />
         </main>
       </div>
     );
