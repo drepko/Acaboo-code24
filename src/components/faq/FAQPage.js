2f3d37043e69adc3d36f2ad8cf41389e472391a3
DucMVu <vmd0210@gmail.com>
2019-04-10 12:18:31 +0200

    update content and image

--- src/components/faq/FAQPage.js
+++ src/components/faq/FAQPage.js
@@ -5,10 +5,19 @@ export default class FAQPage extends Component {
   render() {
     return (
       <div>
-        <h1>Test FAQ</h1>
-        image here
-        <h2>Test h2</h2>
-        <h3>Test h3</h3>
+        <h1>FREQUENTLY ASKED QUESTIONS</h1> <br/>
+        <h3>Can I change my course later on?</h3>
+        <p>No, never ever. Sorry =))</p> <br />
+        <h3>Do you offer discount codes?</h3>
+        <p>Yes, once every 100 years</p> <br />
+        <h3>Are you available in bulk?</h3>
+        <p>Sure, but keep in mind there is no discount for bulk purchase</p> <br />
+        <h3>Can I change my course later on?</h3>
+        <p>No, never ever. Sorry =))</p> <br />
+        <h3>Do you offer discount codes?</h3>
+        <p>Yes, once every 100 years</p> <br />
+        <h3>Are you available in bulk?</h3>
+        <p>Sure, but keep in mind there is no discount for bulk purchase</p> <br />
       </div>
     )
   }
