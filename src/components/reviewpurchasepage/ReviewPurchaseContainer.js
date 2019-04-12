a235441f9e12aa9499371efa2e024513315dcd32
DucMVu <vmd0210@gmail.com>
2019-04-12 15:18:57 +0200

    only Adyen payment gate needs to be integrated

--- src/components/reviewpurchasepage/ReviewPurchaseContainer.js
+++ src/components/reviewpurchasepage/ReviewPurchaseContainer.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import ProgressBar from '../layout/ProgressBar'
+import courses from '../../reducers/courses'
+import { baseUrl } from '../../constants';
 
 class ReviewPurchaseContainer extends PureComponent {
 	constructor(props) {
@@ -14,16 +16,17 @@ class ReviewPurchaseContainer extends PureComponent {
 	}
 
 	render() {
-
 		const {first_name, last_name, email, phone_number} = this.props.currentUser.credentials
+		const {selectedCourses} = this.props
 
 		return (
 			<div>
                 <ProgressBar />
 
 				<h1>Review and Purchase</h1> <br />
-
+				<p>****************</p>
                 <h2>Info</h2>
+
 				<p>
                     First name: {first_name} <br />
                     Last name: {last_name} <br />
@@ -31,11 +34,25 @@ class ReviewPurchaseContainer extends PureComponent {
                     Telephone: {phone_number} <br />
                 </p>
 
+				<p>****************</p>
                 <h2>Selected course(s)</h2>
                 <p>List of selected course</p>
+
+				{
+					selectedCourses.map(course => (
+							<p>
+								Course name: {course.name} <br />
+								Price: €{course.price} <br />
+								<img alt={course.id} src={`${baseUrl}${course.image}`}/>
+							</p>
+						)
+					)	
+				}
+
 				<button onClick={this.handleClick}>Add another course</button>
-				<button>Remove selected course(s) (no logic yet)</button>
+				<button>Remove selected course(s) (no logic yet)</button> <br />
 				
+				<p>****************</p>
                 <h2>Payment</h2>
                 <p>Adyen (looking for a way to integrate)</p>
             </div>
@@ -44,9 +61,9 @@ class ReviewPurchaseContainer extends PureComponent {
 }
 
 const mapStateToProps = state => {
-	console.log('state', state)
 	return {
-		currentUser: state.currentUser
+		currentUser: state.currentUser,
+		selectedCourses: state.selectedCourses
 	}
 }
 
