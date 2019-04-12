ac3b5770a92dc56a951d63089afdf2123c86a079
DucMVu <vmd0210@gmail.com>
2019-04-12 11:55:01 +0200

    update page

--- src/components/reviewpurchasepage/ReviewPurchaseContainer.js
+++ src/components/reviewpurchasepage/ReviewPurchaseContainer.js
@@ -1,18 +1,28 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import ProgressBar from '../layout/ProgressBar'
-// import ReviewPurchase from './ReviewPurchase'
 
 class ReviewPurchaseContainer extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick = () => {
+			this.props.history.push("highlights");
+	}
+
 	render() {
 		const {first_name, last_name, email, phone_number} = this.props.currentUser.credentials
 
 		console.log('props', this.props)
+
 		return (
 			<div>
                 <ProgressBar />
 
 				<h1>Review and Purchase</h1> <br />
+
                 <h2>Info</h2>
 				<p>
                     First name: {first_name} <br />
@@ -20,10 +30,12 @@ class ReviewPurchaseContainer extends PureComponent {
                     Email: {email} <br />
                     Telephone: {phone_number} <br />
                 </p>
+
                 <h2>Selected course(s)</h2>
-                <p>List of selected course, with images to be shown here</p>
+                <p>List of selected course</p>
+				<button onClick={this.handleClick}>Add another course</button>
 				<button>Remove selected course(s) (no logic yet)</button>
-				<button>Add another course (redirect to /courses)</button>
+				
                 <h2>Payment</h2>
                 <p>Adyen (looking for a way to integrate)</p>
             </div>
@@ -32,6 +44,7 @@ class ReviewPurchaseContainer extends PureComponent {
 }
 
 const mapStateToProps = state => {
+	console.log('state', state)
 	return {
 		currentUser: state.currentUser
 	}
