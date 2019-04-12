47b48b84cdf8f74dc420a1ea8dd4c726ced2eb4c
Sandra Padua <sandrapadua5@gmail.com>
2019-04-12 11:22:11 +0200

    about us page in progress

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
+import desklamp from '../../images/mock-ups/deskLamp.png'
 
 export default class AboutPage extends Component {
   render() {
     return (
-      <div>
-        <h1>About Acaboo</h1>
-        <img src='https://source.unsplash.com/random/400x300' />
-        <p>Acaboo started in 2017 with the aim to increase the quality of higher education. Frustrated with the study book market we started with subscription service for digital books. Fast forward to 2019, we can now say that we found new ways to work together with educational publishers. We enable publishers to help with the switch from physical to digital solutions. A new medium comes with new challenges. Publishers have done an amazing job at creating high quality content. We seek to leverage the high quality content of the publisher with our digital solutions. The current generation is consuming content in a different way. They still know how to appreciate good content! It is the way of using it that should change.</p>
+      <div className ="about-us-main">
+        <h3>About Acaboo</h3>
+        <h1>Together we reimagine education </h1>
+        <div className="desk-lamp-about-us">
+            <img className="search_image_1" src={desklamp} />
+          </div>
       </div>
     )
   }
