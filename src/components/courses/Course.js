bc3f3efbc1b77f31cd282f0ec8143b2eb9575682
JewelBreeveld <rjbreeveld@gmail.com>
2019-04-14 18:57:50 +0200

    styling coursepage container

--- src/components/courses/Course.js
+++ src/components/courses/Course.js
@@ -1,17 +1,32 @@
 import React from'react'
 import {baseUrl} from '../../constants'
+import "../../styles/App.css"
 
 export default function Course(props) {
 
         return (
-            <div>
-                <img alt={props.course.name} src={`${baseUrl}${props.course.image}`}/>
-                <p>{props.course.name}</p>
-                <p>{props.course.description}</p>
-                <p>{props.course.price}</p>
-                <button 
-                onClick = {props.signUp} 
-                value = {props.course.id}>Select this course</button>
+            <div className="main-small">
+                <div className="flex-row jc-between">
+                    <div>
+                        <img className="image-large" alt={props.course.name} src={`${baseUrl}${props.course.image}`} />
+                    </div>
+                    <div className="flex-column medium-div">
+                        <div className="text-med-left">
+                            <p>{props.course.name}</p>
+                        </div>
+                        <div>
+                            <p>{props.course.description}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>&#8364; {props.course.price}</p>
+                    </div>
+                    <div>
+                        <button className="btn-white-med right"
+                        onClick = {props.signUp} 
+                        value = {props.course.id}>Select this course</button>
+                    </div>
+                </div> 
             </div>)
     
 }
