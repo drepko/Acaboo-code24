d628d482e095c9e2af001a2522e5e751777b2367
Sandra Padua <sandrapadua5@gmail.com>
2019-04-15 12:25:42 +0200

    changed the text of button offering no courses to course not available

--- src/components/courses/Course.js
+++ src/components/courses/Course.js
@@ -4,8 +4,9 @@ import "../../styles/App.css"
 
 export default function Course(props) {
 
-        return (
-            <div className="main-small">
+    console.log("PROVIDED" ,props.course.provided)
+if(props.course.provided){
+        return (<div className="main-small">
                 <div className="flex-row jc-between">
                     <div>
                         <img className="image-large" alt={props.course.name} src={`${baseUrl}${props.course.image}`} />
@@ -27,7 +28,30 @@ export default function Course(props) {
                         value = {props.course.id}>Select this course</button>
                     </div>
                 </div> 
-            </div>)
-    
+            </div>)  }
+            return (<div className="main-small">
+            <div className="flex-row jc-between">
+                <div>
+                    <img className="image-large" alt={props.course.name} src={`${baseUrl}${props.course.image}`} />
+                </div>
+                <div className="flex-column medium-div">
+                    <div className="text-med-left">
+                        <p>{props.course.name}</p>
+                    </div>
+                    <div>
+                        <p>{props.course.description}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>&#8364; {props.course.price}</p>
+                </div>
+                <div>
+                    <button className="btn-white-med right"
+                    onClick = {props.signUp} 
+                    value = {props.course.id}>Course not available Click to Subscribe</button>
+                </div>
+            </div> 
+        </div>)
+
 }
 
