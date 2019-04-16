8dade3c82ebee3f1b6425fcbd3700f37f43c8c59
DucMVu <vmd0210@gmail.com>
2019-04-16 18:01:16 +0200

    now right price

--- src/reducers/courses.js
+++ src/reducers/courses.js
@@ -3,7 +3,15 @@ import { UPDATE_COURSES, GET_HIGHLIGHTS } from '../actions/courses'
 export default (state = null, { type, payload }) => {
   switch (type) {
     case UPDATE_COURSES:
-      return payload
+      return payload.map(course => ({
+        description: course.description,
+        id: course.id,
+        image: course.image,
+        name: course.name,
+        price: course.price ? course.price/100 : null,
+        provided: course.provided
+        })
+      )
     case GET_HIGHLIGHTS:
       return payload
     default:
