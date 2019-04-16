b1fedbcac39917ee08ac40e957d34a6ea8123b73
Danielle <repko.dm@gmail.com>
2019-04-16 18:08:15 +0200

    image man-handshake is next to text and scaled now

--- src/components/homepage/LandingsPageContainer.js
+++ src/components/homepage/LandingsPageContainer.js
@@ -4,8 +4,8 @@ import FindCourseContainer from './FindCourseContainer';
 import woman_desk from '../../images/mock-ups/woman_desk.png'
 import Universiteit_Leiden from '../../images/universities/Universiteit_Leiden.png'
 import UvA from '../../images/universities/UvA.png'
-import man_handshake from'../../images/mock-ups/man_handshake.png'
-import people from'../../images/mock-ups/people.png'
+import man_handshake from '../../images/mock-ups/man_handshake.png'
+import people from '../../images/mock-ups/people.png'
 
 import {
   home_studydashboard,
@@ -20,26 +20,26 @@ class LandingsPageContainer extends React.Component {
     return (
       <div className="main">
 
-        <div className="flex-row fw jc-center ai-start-reverse"> 
-        
+        <div className="flex-row fw jc-center ai-start-reverse">
+
 
           <div className="flex-column half-width padding-top">
-          
+
             <div className="half-width ai-start">
 
               <div className="half-width ai-start">
-                <p className="text-xl-black"> Pass your exam <br/>with ease
+                <p className="text-xl-black"> Pass your exam <br />with ease
                 </p>
               </div>
 
               <div className="half-width ai-start">
                 <p className="text-sm-med-grey">
-                  Sed ut perspiciatis unde omnis iste natus <br/>error sit voluptatem accusantium.
+                  Sed ut perspiciatis unde omnis iste natus <br />error sit voluptatem accusantium.
                 </p>
-              </div> 
+              </div>
 
             </div>
-                <FindCourseContainer history={this.props.history} />
+            <FindCourseContainer history={this.props.history} />
           </div>
 
           <div className="half-width">
@@ -47,7 +47,7 @@ class LandingsPageContainer extends React.Component {
           </div>
 
         </div>
-        
+
 
         <div className="flex-column jc">
           <p className="text-xs-black">TRUSTED BY STUDENTS FROM:</p>
@@ -65,46 +65,64 @@ class LandingsPageContainer extends React.Component {
         </div>
 
         <div className="text-center">
-            <p className="text-lg-black">Study more efficiently with Acaboo.</p>
-            <p className="text-sm-med-grey">Sed ut perspiciatis unde omnis iste natus error sit voluptatem<br />
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+          <p className="text-lg-black">Study more efficiently with Acaboo.</p>
+          <p className="text-sm-med-grey">Sed ut perspiciatis unde omnis iste natus error sit voluptatem<br />
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
         </div>
 
         <div className="padding-top">
           <img alt="image" className="image-lg-overflow shadow" src={home_studydashboard} />
         </div>
 
-        <div className="flex-row" >
-       
-          <div className="flex-column ai-start small-width">
+        <div className="flex-row about-box padding-top" >
 
-            <div className="">
-              <p className="text-med-black">About Acaboo</p>
-              <p className="text-sm-grey">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
-              <Link to="/about" className="text-med">Learn more &#8594;</Link>
+          <div className="flex-column ai-start small-width padding-top">
+
+            <div id="about-and-picture">
+              <div id='about-flex-child-one'>
+
+                {/* <div className="text-box-med">
+                  <p className="text-med-black">About Acaboo</p>
+                  <p className="text-sm-grey">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                  <Link to="/about" className="text-med">Learn more &#8594;</Link>
+                </div> */}
+
+                <div className="text-box-med">
+                  <p className="text-med-black">About Acaboo</p>
+                  <p className="text-sm-grey"> Acaboo is an online study book<br/> 
+                  platform with study books from</p>
+                  <Link to="/about" className="text-med">Learn more &#8594;</Link>
+                </div>
+
+                <div className="text-box-med">
+                  <p className="text-sm-black">Feature 1</p>
+                  <p className="text-sm-grey">  Sed ut perspiciatis unde omnis iste <br />
+                    natus error sit voluptatem<br />
+                    accusantium.</p>
+                </div>
+                <div className="text-box-med">
+                  <p className="text-sm-black">Feature 2</p>
+                  <p className="text-sm-grey">  Sed ut perspiciatis unde omnis iste <br />
+                    natus error sit voluptatem <br />
+                    accusantium.</p>
+                </div>
+              </div>
+
+              <div id='about-flex-child-two'>
+                <div className="">
+                  <img alt="image" className="image-lg-overflow" src={man_handshake} />
+                </div>
+              </div>
+
             </div>
-            <div className="text-box-med">
-              <p className="text-sm-black">Feature 1</p>
-              <p className="text-sm-grey">  Sed ut perspiciatis unde omnis iste <br />
-                natus error sit voluptatem<br />
-                accusantium.</p>
-            </div>
-            <div className="text-box-med">
-              <p className="text-sm-black">Feature 2</p>
-              <p className="text-sm-grey">  Sed ut perspiciatis unde omnis iste <br />
-                natus error sit voluptatem <br />
-                accusantium.</p>
-            </div>
-          
+            {/* </div>   */}
           </div>
 
-          <div className="med-width">
-            <img alt="image" className="image-lg-overflow" src={man_handshake} /> 
-          </div> 
+
 
         </div>
 
-        <BrowseCourse/>
+        <BrowseCourse />
 
         <Testimonial />
 
@@ -113,7 +131,7 @@ class LandingsPageContainer extends React.Component {
           <p className="font-grey">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
         </div>
 
-      <div className="section_9 section_image_people">
+        <div className="section_9 section_image_people">
           <img alt="image" className="image-lg-overflow" src={people} />
         </div>
 
@@ -121,15 +139,15 @@ class LandingsPageContainer extends React.Component {
           <h1 className="text-lg-black">Start studying with Acaboo.</h1>
           <div className="flex-row jc">
             <div>
-            <Link to="/courses"><button className="btn-white-med">Browse courses</button></Link>
+              <Link to="/courses"><button className="btn-white-med">Browse courses</button></Link>
             </div>
             <div>
-            <Link to="/about"><button className="btn-blue-med">Learn More</button></Link>  
+              <Link to="/about"><button className="btn-blue-med">Learn More</button></Link>
             </div>
           </div>
           <p>Add some helper text here to explain the finer details of your product or service.</p>
         </div>
-    </div>)
+      </div>)
   }
 }
 
