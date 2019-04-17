23ffe467d1ea5c9a56b90422ea804821348c9d58
JewelBreeveld <rjbreeveld@gmail.com>
2019-04-17 17:56:00 +0200

    styling coursepage is mostly done, only the cart still needs styling

--- src/components/courses/Course.js
+++ src/components/courses/Course.js
@@ -2,17 +2,18 @@ import React from'react'
 import {baseUrl} from '../../constants'
 import "../../styles/App.css"
 import EmailwhenAvailable from './EmailWhenAvailable'
+import no_image from '../../images/no_image.svg'
 
 
 export default function Course(props) {
 
     if(props.course.provided){
-        return (<div className="main-row">
+        return (<div className="main-row margin-bottom">
                     <div className="main-row border-thin-grey max-width-med">
                         <div className="padding-side-small">
                             <img className="image-book" alt={props.course.name} src={`${baseUrl}${props.course.image}`} />
                         </div>
-                        <div className="flex-column medium-div ai-start">
+                        <div className="flex-column medium-div ai-start padding-tb">
                             <div className="text-sm-med-black text-start">
                                 <p>{props.course.name}</p>
                             </div>
@@ -20,36 +21,45 @@ export default function Course(props) {
                                 <p>{props.course.description}</p>
                             </div>
                         </div>
-                        <div className="text-sm-med-black padding-left">
+                        <div className="text-sm-med-black">
                             <p>&#8364; {props.course.price.toFixed(2).toString().replace(".", ",")}</p>
                         </div>
                     </div> 
                     <div className="padding-side-small max-width-xsmall">
-                            <button className="btn-white-med text-sm-blue"
+                            <button className="btn-white-med text-sm-blue margin-side"
                             onClick = {props.signUp} 
                             value = {props.course.id}>Select course</button>
                     </div>
                 </div>)  }
 
-        return (<div className="main-small">
-                    <div className="flex-row jc-between">
-                        <div className="flex-column">
-                            <div className="text-med-left">
-                                {/* <p>{props.course.name}</p> */}
+        return (<div className="main-row margin-bottom">
+                    <div className="main-row border-thin-grey max-width-med">
+                        
+                        <div className="padding-side-small">
+                            {/* <img className="image-book" alt={props.course.name} src={no_image} /> */}
+                        </div>
+                        
+                        <div className="flex-column medium-div ai-start padding-tb">
+                            <div className="text-sm-med-black text-start">
+                                <p>{props.course.name}</p>
                             </div>
-                            <div>
+
+                            <div className="max-width-small text-sm-grey">
                                 <p>{props.course.description}</p>
                             </div>
+
                             <EmailwhenAvailable 
                             id={props.course.id}
                             course={props.course.name}
                             />
                         </div>
-                        <div>
-                            <p>Not Available</p>
+
+                    </div>
+                        <div className="padding-side-small max-width-xsmall ">
+                            <p className="text-sm-black">Unavailable</p>
                         </div>
                     </div> 
-                </div>)
+                )
 
 }
 
