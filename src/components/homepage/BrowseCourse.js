89720ba1349a626092068a10a4f4de8de8d2f85f
CurlyBracket1990 <stefan_moes@live.nl>
2019-04-18 10:09:40 +0200

    Changed color, border and size/padding

--- src/components/homepage/BrowseCourse.js
+++ src/components/homepage/BrowseCourse.js
@@ -7,7 +7,7 @@ export default class BrowseCourse extends Component {
       <div className="med-width padding-top" > 
         <div className="ai-start-reverse med-width">
           <div className="flex-row jc-between bg-blue padding-side-small med-width half-height">
-              <p className="text-med-white">Start studying with Acaboo</p>
+              <p className="text-med-white padding-top-small">Start studying with Acaboo</p>
               <Link to="/courses"><button className="btn-white-lg text-sm-blue padding-side-small">Browse courses</button></Link>
           </div>
 
