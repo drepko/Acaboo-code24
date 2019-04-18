3c4966f73b0456657d0ad45fc5459cbf8665c7e6
JewelBreeveld <rjbreeveld@gmail.com>
2019-04-18 15:09:39 +0200

    work in progress... review and purchase page

--- src/components/reviewpurchasepage/ReviewPurchaseContainer.js
+++ src/components/reviewpurchasepage/ReviewPurchaseContainer.js
@@ -29,37 +29,46 @@ class ReviewPurchaseContainer extends PureComponent {
 		return (
 			<div>
                 <ProgressBar />
-
 				<p className="text-med-black padding-top border-bottom-grey">Review and Purchase</p> <br />
-				
-				<div>
-					<div>
-						<p className="text-med-black">Info</p>
-						<p className="text-sm-black">
-							First name: {first_name} <br />
-							Last name: {last_name} <br />
-							Email: {email} <br />
-							Telephone: {phone_number} <br />
-						</p>
+				<div className="display-flex-start">
+					<div className="main-row ai-start">
+
+						<div>
+							<p className="text-med-black">Info</p>
+							<p className="text-sm-black">
+								First name: <b>{first_name}</b> <br />
+								Last name: <b>{last_name}</b> <br />
+								Email: <b>{email}</b> <br />
+								Telephone: <b>{phone_number}</b> <br />
+							</p>
+						</div>
+
+						<div className="max-width-med">
+							<p className="text-med-black">Course</p>
+							<div className="main-row border-thin-grey max-width-med">
+								{selectedCourses.map(course => 
+								(<div>
+									<div className="padding-side-small">
+										<img className="image-book" alt={course.id} src={`${baseUrl}${course.image}`}/>
+									</div>
+									<div className="flex-column  medium-div ai-start padding-tb">
+										<p>Course name: <b>{course.name}</b></p>
+										<p>Price: <b>€ &nbsp;{course.price.toFixed(2).toString().replace(".", ",")}</b></p>
+									</div>
+								</div>))}							
+							</div>
+						</div>
+
+					</div>
+
+					<div className="ai-center padding">
+					{selectedCourses.map(course => (
+						<button className="btn-white-med text-sm-blue margin-side" onClick={this.onDelete} value={course.id}>Remove</button>))}
 					</div>
 
 				</div>
                 
 
-				<p>****************</p>
-                <h2>Selected course(s)</h2>
-
-				{
-					selectedCourses.map(course => (
-							<p>
-								Course name: {course.name} <br />
-								Price: €{course.price.toFixed(2).toString().replace(".", ",")} <br />
-								<img alt={course.id} src={`${baseUrl}${course.image}`}/> <br />
-								<button onClick={this.onDelete} value={course.id}>Remove this course</button> <br />
-							</p>
-						)
-					)	
-				}
 
 				<p>****************</p>
 				{
