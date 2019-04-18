565a2bc743253e1e7dd3c30a562a734bbe21afb5
JewelBreeveld <rjbreeveld@gmail.com>
2019-04-18 13:12:55 +0200

    added the correct pictures to the we're hiring page

--- src/components/about/AboutUsButton.js
+++ src/components/about/AboutUsButton.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
  export default class AboutUsButton extends React.Component {
 render(){
     return(
-        <div>
+        <div className="">
             <p className="text-lg-black">Reimagine education with us</p>
-            <Link to="/about"><button className="btn-blue-xl">Learn more about Acaboo</button></Link> 
+            <Link to="/about"><button className="btn-blue-xl text-sm-white">Learn more about Acaboo</button></Link> 
         </div>
     )
 }
