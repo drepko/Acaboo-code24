bb49194b64bbb74aa4144809eed2abd0ec674d0a
Sandra Padua <sandrapadua5@gmail.com>
2019-04-19 09:54:39 +0200

    about page styling

--- src/components/about/JoinUs.js
+++ src/components/about/JoinUs.js
@@ -7,8 +7,8 @@ export default class Team extends Component {
     return (
       <div className ="text-box-med">
         <p className ='text-med-black font-weight-bold'>Join Us </p>
-        <p className ='text-sm-med-black'>Acaboo is based in the beautiful Amsterdam. We are a diverse group of people interested in education, tecnology and the future of life!</p>
-        <p>Interested in joining us? <Link to = '/careers'>Learn more here</Link></p>
+        <p className ='text-sm-black'>Acaboo is based in the beautiful Amsterdam. We are a diverse group of people interested in education, tecnology and the future of life!</p>
+        <p className ='text-sm-black'>Interested in joining us? <Link to = '/careers'>Learn more here</Link></p>
 
       </div>
     )
